1f29e33e8f352ee06bc97d6d1dd023915be932d5
kiri_feather <kiri_feather@users.sourceforge.jp>
2008-10-12 00:32:38 +0000

    MyCommon.vbがリポジトリから漏れていたので追加
    git-svn-id: http://svn.coderepos.org/share/lang/vb2005/Tween@21157 d0d07461-0603-4401-acd4-de1884942a52

---
+++ Tween/MyCommon.vb
@@ -0,0 +1,32 @@
+﻿Public Module MyCommon
+    Public Enum IconSizes
+        IconNone = 0
+        Icon16 = 1
+        Icon24 = 2
+        Icon48 = 3
+        Icon48_2 = 4
+    End Enum
+
+    Public Enum NameBalloonEnum
+        None
+        UserID
+        NickName
+    End Enum
+
+    Public Enum DispTitleEnum
+        None
+        Ver
+        Post
+        UnreadRepCount
+        UnreadAllCount
+        UnreadAllRepCount
+        UnreadCountAllCount
+    End Enum
+
+    Public Enum LogUnitEnum
+        Minute
+        Hour
+        Day
+    End Enum
+
+End Module
