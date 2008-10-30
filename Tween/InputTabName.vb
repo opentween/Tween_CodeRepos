efb0cf5f7157f80cf8ec18881f66b301fe21a547
kiri_feather <kiri_feather@users.sourceforge.jp>
2008-10-30 00:14:49 +0000

    InputTabName追加
    git-svn-id: http://svn.coderepos.org/share/lang/vb2005/Tween@22349 d0d07461-0603-4401-acd4-de1884942a52

---
+++ Tween/InputTabName.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Windows.Forms
+
+Public Class InputTabName
+
+    Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK_Button.Click
+        Me.DialogResult = System.Windows.Forms.DialogResult.OK
+        Me.Close()
+    End Sub
+
+    Private Sub Cancel_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel_Button.Click
+        TextTabName.Text = ""
+        Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Close()
+    End Sub
+
+    Public Property TabName() As String
+        Get
+            Return Me.TextTabName.Text.Trim()
+        End Get
+        Set(ByVal value As String)
+            TextTabName.Text = value.Trim()
+        End Set
+    End Property
+
+End Class
