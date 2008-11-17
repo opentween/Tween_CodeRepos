6c5fcdeea17aa3888460e4dc83d0a54f334f0917
takeshik <takeshik@users.sourceforge.jp>
2008-11-17 04:37:18 +0000

    ライセンス告知の追加 (分割コミット)
    git-svn-id: http://svn.coderepos.org/share/lang/vb2005/Tween@23887 d0d07461-0603-4401-acd4-de1884942a52

--- Tween/OpenURL.vb
+++ Tween/OpenURL.vb
@@ -1,4 +1,27 @@
-﻿Imports System.Windows.Forms
+﻿' Tween - Client of Twitter
+' Copyright © 2007-2008 kiri_feather (@kiri_feather) <kiri_feather@gmail.com>
+'           © 2008      Moz (@syo68k) <http://iddy.jp/profile/moz/>
+'           © 2008      takeshik (@takeshik) <http://www.takeshik.org/>
+' All rights reserved.
+' 
+' This file is part of Tween.
+' 
+' This program is free software; you can redistribute it and/or modify it
+' under the terms of the GNU General Public License as published by the Free
+' Software Foundation; either version 3 of the License, or (at your option)
+' any later version.
+' 
+' This program is distributed in the hope that it will be useful, but
+' WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+' or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+' for more details. 
+' 
+' You should have received a copy of the GNU General Public License along
+' with this program. If not, see <http://www.gnu.org/licenses/>, or write to
+' the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+' Boston, MA 02110-1301, USA.
+
+Imports System.Windows.Forms
 
 Public Class OpenURL
 
