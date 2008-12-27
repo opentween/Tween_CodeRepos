5cacd51a7360ba277e222b40e7e154e8c1ba0aa6
syo68k <syo68k@users.sourceforge.jp>
2008-12-27 09:35:42 +0000

    0200リリース。忘れていたのでコミット
    git-svn-id: http://svn.coderepos.org/share/lang/vb2005/Tween@27486 d0d07461-0603-4401-acd4-de1884942a52

--- Tween/My Project/AssemblyInfo.vb
+++ Tween/My Project/AssemblyInfo.vb
@@ -54,6 +54,6 @@ Imports System.Runtime.InteropServices
 ' 既定値にすることができます:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.1.9.1")> 
-<Assembly: AssemblyFileVersion("0.1.9.1")> 
+<Assembly: AssemblyVersion("0.2.0.0")> 
+<Assembly: AssemblyFileVersion("0.2.0.0")> 
 
