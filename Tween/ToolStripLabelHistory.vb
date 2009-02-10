2584c450e4ffaf6ed7b3becdcd0fcb4fa2f1b966
kiri_feather <kiri_feather@users.sourceforge.jp>
2009-02-10 14:11:39 +0000

    履歴用ToolStripLabel追加漏れ
    git-svn-id: http://svn.coderepos.org/share/lang/vb2005/Tween@29867 d0d07461-0603-4401-acd4-de1884942a52

---
+++ Tween/ToolStripLabelHistory.vb
@@ -0,0 +1,25 @@
+﻿Public Class ToolStripLabelHistory
+    Inherits ToolStripLabel
+
+    Private sList As New List(Of String)
+    Private Const MAXCNT As Integer = 10
+
+    Public Overrides Property Text() As String
+        Get
+            Return MyBase.Text
+        End Get
+        Set(ByVal value As String)
+            sList.Add(value)
+            Do While sList.Count > MAXCNT
+                sList.RemoveAt(0)
+            Loop
+            Dim hst As String = ""
+            For Each sts As String In sList
+                If hst <> "" Then hst += System.Environment.NewLine
+                hst += sts
+            Next
+            Me.ToolTipText = hst
+            MyBase.Text = value
+        End Set
+    End Property
+End Class
