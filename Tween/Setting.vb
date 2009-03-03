537d9373f218c55525917cf6aa17e213510768b0
takeshik <takeshik@users.sourceforge.jp>
2009-03-03 07:17:53 +0000

    XmlConfiguration#ReadXml() のネスト時の挙動を修正
    git-svn-id: http://svn.coderepos.org/share/lang/vb2005/Tween@30724 d0d07461-0603-4401-acd4-de1884942a52

--- Tween/Setting.vb
+++ Tween/Setting.vb
@@ -1608,7 +1608,7 @@ Public NotInheritable Class XmlConfiguration
         Dim xdoc As XmlDocument = New XmlDocument()
 
         xdoc.Load(reader)
-        For Each xentryNode As XmlNode In xdoc.SelectNodes("//entry")
+        For Each xentryNode As XmlNode In xdoc.DocumentElement.GetElementsByTagName("entry")
             Dim xentry As XmlElement = CType(xentryNode, XmlElement)
             Me.Add( _
                 xentry.Attributes.ItemOf("key").Value, _
@@ -1723,31 +1723,6 @@ Public NotInheritable Class XmlConfiguration
     End Function
 
 End Class
-
-' Tween - Client of Twitter
-' Copyright © 2007-2009 kiri_feather (@kiri_feather) <kiri_feather@gmail.com>
-'           © 2008-2009 Moz (@syo68k) <http://iddy.jp/profile/moz/>
-'           © 2008-2009 takeshik (@takeshik) <http://www.takeshik.org/>
-' All rights reserved.
-' 
-' This file is part of Tween.
-' 
-' This program is free software; you can redistribute it and/or modify it
-' under the terms of the GNU General Public License as published by the Free
-' Software Foundation; either version 3 of the License, or (at your option)
-' any later version.
-' 
-' This program is distributed in the hope that it will be useful, but
-' WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-' or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
-' for more details. 
-' 
-' You should have received a copy of the GNU General Public License along
-' with this program. If not, see <http://www.gnu.org/licenses/>, or write to
-' the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
-' Boston, MA 02110-1301, USA.
-
-
 
 Public NotInheritable Class SettingBase
 
