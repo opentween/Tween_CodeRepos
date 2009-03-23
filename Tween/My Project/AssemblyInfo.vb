cbd1f0083c80ef80ea3d7b995717b981456f23c9
syo68k <syo68k@users.sourceforge.jp>
2009-03-23 04:50:39 +0000

    設定保存タイミング見直し
    git-svn-id: http://svn.coderepos.org/share/lang/vb2005/Tween@31462 d0d07461-0603-4401-acd4-de1884942a52

--- Tween/My Project/AssemblyInfo.vb
+++ Tween/My Project/AssemblyInfo.vb
@@ -54,6 +54,6 @@ Imports System.Runtime.InteropServices
 ' 既定値にすることができます:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.3.7.0")> 
-<Assembly: AssemblyFileVersion("0.3.7.0")> 
+<Assembly: AssemblyVersion("0.3.8.0")> 
+<Assembly: AssemblyFileVersion("0.3.8.0")> 
 
