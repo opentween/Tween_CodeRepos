c03189db86c42e27370ac6b41d7a52100d950a9b
kiri_feather <kiri_feather@users.sourceforge.jp>
2009-04-03 01:55:51 +0000

    0391Trunk。変更履歴とリビジョンアップ
    git-svn-id: http://svn.coderepos.org/share/lang/vb2005/Tween@31846 d0d07461-0603-4401-acd4-de1884942a52

--- Tween/My Project/AssemblyInfo.vb
+++ Tween/My Project/AssemblyInfo.vb
@@ -54,6 +54,6 @@ Imports System.Runtime.InteropServices
 ' 既定値にすることができます:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.3.9.0")> 
-<Assembly: AssemblyFileVersion("0.3.9.0")> 
+<Assembly: AssemblyVersion("0.3.9.1")> 
+<Assembly: AssemblyFileVersion("0.3.9.1")> 
 
