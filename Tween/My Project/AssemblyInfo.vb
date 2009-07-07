3e32d86b55606246c055fc9de315e546f65cad84
kiri_feather <kiri_feather@users.sourceforge.jp>
2009-07-07 09:44:28 +0000

    preタグに変更 follower取得非同期化 Webモードfollower取得のキャッシュを使わないように変更（常に全件取得） APIモードのfollower取得で、5000件を超えても取得できるように。（Webモードも対応必要？） ハッシュタグのリンク化で改行を含めてリンクしていたバグ修正
    git-svn-id: http://svn.coderepos.org/share/lang/vb2005/Tween/trunk@34339 d0d07461-0603-4401-acd4-de1884942a52

--- Tween/My Project/AssemblyInfo.vb
+++ Tween/My Project/AssemblyInfo.vb
@@ -54,6 +54,6 @@ Imports System.Runtime.InteropServices
 ' 既定値にすることができます:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.5.5.1")> 
-<Assembly: AssemblyFileVersion("0.5.5.1")> 
+<Assembly: AssemblyVersion("0.5.5.2")> 
+<Assembly: AssemblyFileVersion("0.5.5.2")> 
 
