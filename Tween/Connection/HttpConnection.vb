55ce26933b4c8d95bb26d511af5bd6c2b9e26157
kiri_feather <kiri_feather@users.sourceforge.jp>
2009-08-03 17:29:13 +0000

    通信部置き換え（途中）
    git-svn-id: http://svn.coderepos.org/share/lang/vb2005/Tween/trunk@34730 d0d07461-0603-4401-acd4-de1884942a52

--- Tween/Connection/HttpConnection.vb
+++ Tween/Connection/HttpConnection.vb
@@ -4,6 +4,7 @@ Public Class HttpConnection
     Protected Shared _defaultTimeOut As Integer = 20000
     Protected Shared _proxy As System.Net.WebProxy = Nothing
     Protected Shared _proxyType As ProxyTypeEnum = ProxyTypeEnum.IE
+    Private Shared _cookieContainer As New CookieContainer
 
     Protected Enum WEBACCESS_REQ_TYPE
         ReqGET
@@ -38,9 +39,69 @@ Public Class HttpConnection
                 writer.Write(CreateQueryString(param))
             End Using
         End If
+        webReq.CookieContainer = _cookieContainer
 
         Return webReq
     End Function
+
+    Protected Shared Function GetResponse(ByVal webRequest As HttpWebRequest, _
+                                        ByVal resultStream As System.IO.Stream, _
+                                        ByVal headerInfo As System.Collections.Generic.Dictionary(Of String, String) _
+                                    ) As HttpStatusCode
+        Using webRes As HttpWebResponse = CType(webRequest.GetResponse(), HttpWebResponse)
+            GetHeaderInfo(webRes, headerInfo)
+
+            Dim statusCode As HttpStatusCode = webRes.StatusCode
+            If statusCode = HttpStatusCode.MovedPermanently OrElse _
+               statusCode = HttpStatusCode.Found OrElse _
+               statusCode = HttpStatusCode.SeeOther OrElse _
+               statusCode = HttpStatusCode.TemporaryRedirect Then
+                If headerInfo.ContainsKey("Location") Then
+                    headerInfo.Item("Location") = webRes.Headers.Item("Location")
+                Else
+                    headerInfo.Add("Location", webRes.Headers.Item("Location"))
+                End If
+                Return statusCode
+            End If
+
+            Using strm As System.IO.Stream = webRes.GetResponseStream()
+                If strm IsNot Nothing Then CopyStream(strm, resultStream)
+            End Using
+            Return statusCode
+        End Using
+    End Function
+
+    Private Shared Sub CopyStream(ByVal inStream As System.IO.Stream, _
+                                            ByVal outStream As System.IO.Stream)
+        If inStream Is Nothing Then Throw New ArgumentNullException("Input stream is null.")
+        If outStream Is Nothing Then Throw New ArgumentNullException("Output stream is null.")
+        If Not inStream.CanRead Then Throw New ArgumentException("Input stream can not read.")
+        If Not outStream.CanWrite Then Throw New ArgumentException("Output stream can not write.")
+        If inStream.CanSeek AndAlso inStream.Length = 0 Then Throw New ArgumentException("Input stream do not have data.")
+
+        Dim buffer(1023) As Byte
+        Dim i As Integer = buffer.Length
+        Do
+            i = inStream.Read(buffer, 0, i)
+            If i = 0 Then Exit Do
+            outStream.Write(buffer, 0, i)
+        Loop
+    End Sub
+
+    Private Shared Sub GetHeaderInfo(ByVal webResponse As HttpWebResponse, _
+                            ByVal headerInfo As System.Collections.Generic.Dictionary(Of String, String))
+        If headerInfo IsNot Nothing AndAlso headerInfo.Count > 0 Then
+            Dim keys(headerInfo.Count - 1) As String
+            headerInfo.Keys.CopyTo(keys, 0)
+            For Each key As String In keys
+                If Array.IndexOf(webResponse.Headers.AllKeys, key) > -1 Then
+                    headerInfo.Item(key) = webResponse.Headers.Item(key)
+                Else
+                    headerInfo.Item(key) = ""
+                End If
+            Next
+        End If
+    End Sub
 
     Protected Shared Function CreateQueryString(ByVal param As System.Collections.Generic.SortedList(Of String, String)) As String
         If param Is Nothing OrElse param.Count = 0 Then Return String.Empty
