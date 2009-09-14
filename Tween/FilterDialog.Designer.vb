2500237c11688f07094a12acdf7d23bd1794b26a
kiri_feather <kiri_feather@users.sourceforge.jp>
2009-09-14 10:54:54 +0000

    タブオーダー修正 更新履歴の誤字修正
    git-svn-id: http://svn.coderepos.org/share/lang/vb2005/Tween/trunk@35370 d0d07461-0603-4401-acd4-de1884942a52

--- Tween/FilterDialog.Designer.vb
+++ Tween/FilterDialog.Designer.vb
@@ -55,50 +55,31 @@ Partial Class FilterDialog
         '
         'ButtonClose
         '
-        Me.ButtonClose.AccessibleDescription = Nothing
-        Me.ButtonClose.AccessibleName = Nothing
-        resources.ApplyResources(Me.ButtonClose, "ButtonClose")
-        Me.ButtonClose.BackgroundImage = Nothing
         Me.ButtonClose.DialogResult = System.Windows.Forms.DialogResult.Cancel
-        Me.ButtonClose.Font = Nothing
+        resources.ApplyResources(Me.ButtonClose, "ButtonClose")
         Me.ButtonClose.Name = "ButtonClose"
         Me.ButtonClose.UseVisualStyleBackColor = True
         '
         'ComboTabs
         '
-        Me.ComboTabs.AccessibleDescription = Nothing
-        Me.ComboTabs.AccessibleName = Nothing
-        resources.ApplyResources(Me.ComboTabs, "ComboTabs")
-        Me.ComboTabs.BackgroundImage = Nothing
         Me.ComboTabs.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-        Me.ComboTabs.Font = Nothing
         Me.ComboTabs.FormattingEnabled = True
+        resources.ApplyResources(Me.ComboTabs, "ComboTabs")
         Me.ComboTabs.Name = "ComboTabs"
         '
         'Label5
         '
-        Me.Label5.AccessibleDescription = Nothing
-        Me.Label5.AccessibleName = Nothing
         resources.ApplyResources(Me.Label5, "Label5")
-        Me.Label5.Font = Nothing
         Me.Label5.Name = "Label5"
         '
         'ListFilters
         '
-        Me.ListFilters.AccessibleDescription = Nothing
-        Me.ListFilters.AccessibleName = Nothing
-        resources.ApplyResources(Me.ListFilters, "ListFilters")
-        Me.ListFilters.BackgroundImage = Nothing
-        Me.ListFilters.Font = Nothing
         Me.ListFilters.FormattingEnabled = True
+        resources.ApplyResources(Me.ListFilters, "ListFilters")
         Me.ListFilters.Name = "ListFilters"
         '
         'EditFilterGroup
         '
-        Me.EditFilterGroup.AccessibleDescription = Nothing
-        Me.EditFilterGroup.AccessibleName = Nothing
-        resources.ApplyResources(Me.EditFilterGroup, "EditFilterGroup")
-        Me.EditFilterGroup.BackgroundImage = Nothing
         Me.EditFilterGroup.Controls.Add(Me.GroupBox1)
         Me.EditFilterGroup.Controls.Add(Me.CheckURL)
         Me.EditFilterGroup.Controls.Add(Me.CheckRegex)
@@ -113,214 +94,135 @@ Partial Class FilterDialog
         Me.EditFilterGroup.Controls.Add(Me.RadioPLUS)
         Me.EditFilterGroup.Controls.Add(Me.Label8)
         Me.EditFilterGroup.Controls.Add(Me.ButtonOK)
-        Me.EditFilterGroup.Font = Nothing
+        resources.ApplyResources(Me.EditFilterGroup, "EditFilterGroup")
         Me.EditFilterGroup.Name = "EditFilterGroup"
         Me.EditFilterGroup.TabStop = False
         '
         'GroupBox1
         '
-        Me.GroupBox1.AccessibleDescription = Nothing
-        Me.GroupBox1.AccessibleName = Nothing
-        resources.ApplyResources(Me.GroupBox1, "GroupBox1")
-        Me.GroupBox1.BackgroundImage = Nothing
         Me.GroupBox1.Controls.Add(Me.OptNone)
         Me.GroupBox1.Controls.Add(Me.OptMark)
         Me.GroupBox1.Controls.Add(Me.OptMove)
-        Me.GroupBox1.Font = Nothing
+        resources.ApplyResources(Me.GroupBox1, "GroupBox1")
         Me.GroupBox1.Name = "GroupBox1"
         Me.GroupBox1.TabStop = False
         '
         'OptNone
         '
-        Me.OptNone.AccessibleDescription = Nothing
-        Me.OptNone.AccessibleName = Nothing
         resources.ApplyResources(Me.OptNone, "OptNone")
-        Me.OptNone.BackgroundImage = Nothing
-        Me.OptNone.Font = Nothing
         Me.OptNone.Name = "OptNone"
         Me.OptNone.TabStop = True
         Me.OptNone.UseVisualStyleBackColor = True
         '
         'OptMark
         '
-        Me.OptMark.AccessibleDescription = Nothing
-        Me.OptMark.AccessibleName = Nothing
         resources.ApplyResources(Me.OptMark, "OptMark")
-        Me.OptMark.BackgroundImage = Nothing
-        Me.OptMark.Font = Nothing
         Me.OptMark.Name = "OptMark"
         Me.OptMark.TabStop = True
         Me.OptMark.UseVisualStyleBackColor = True
         '
         'OptMove
         '
-        Me.OptMove.AccessibleDescription = Nothing
-        Me.OptMove.AccessibleName = Nothing
         resources.ApplyResources(Me.OptMove, "OptMove")
-        Me.OptMove.BackgroundImage = Nothing
-        Me.OptMove.Font = Nothing
         Me.OptMove.Name = "OptMove"
         Me.OptMove.TabStop = True
         Me.OptMove.UseVisualStyleBackColor = True
         '
         'CheckURL
         '
-        Me.CheckURL.AccessibleDescription = Nothing
-        Me.CheckURL.AccessibleName = Nothing
         resources.ApplyResources(Me.CheckURL, "CheckURL")
-        Me.CheckURL.BackgroundImage = Nothing
-        Me.CheckURL.Font = Nothing
         Me.CheckURL.Name = "CheckURL"
         Me.CheckURL.UseVisualStyleBackColor = True
         '
         'CheckRegex
         '
-        Me.CheckRegex.AccessibleDescription = Nothing
-        Me.CheckRegex.AccessibleName = Nothing
         resources.ApplyResources(Me.CheckRegex, "CheckRegex")
-        Me.CheckRegex.BackgroundImage = Nothing
-        Me.CheckRegex.Font = Nothing
         Me.CheckRegex.Name = "CheckRegex"
         Me.CheckRegex.UseVisualStyleBackColor = True
         '
         'RadioAND
         '
-        Me.RadioAND.AccessibleDescription = Nothing
-        Me.RadioAND.AccessibleName = Nothing
         resources.ApplyResources(Me.RadioAND, "RadioAND")
-        Me.RadioAND.BackgroundImage = Nothing
         Me.RadioAND.Checked = True
-        Me.RadioAND.Font = Nothing
         Me.RadioAND.Name = "RadioAND"
         Me.RadioAND.TabStop = True
         Me.RadioAND.UseVisualStyleBackColor = True
         '
         'ButtonCancel
         '
-        Me.ButtonCancel.AccessibleDescription = Nothing
-        Me.ButtonCancel.AccessibleName = Nothing
         resources.ApplyResources(Me.ButtonCancel, "ButtonCancel")
-        Me.ButtonCancel.BackgroundImage = Nothing
-        Me.ButtonCancel.Font = Nothing
         Me.ButtonCancel.Name = "ButtonCancel"
         Me.ButtonCancel.UseVisualStyleBackColor = True
         '
         'UID
         '
-        Me.UID.AccessibleDescription = Nothing
-        Me.UID.AccessibleName = Nothing
         resources.ApplyResources(Me.UID, "UID")
-        Me.UID.BackgroundImage = Nothing
-        Me.UID.Font = Nothing
         Me.UID.Name = "UID"
         '
         'MSG2
         '
-        Me.MSG2.AccessibleDescription = Nothing
-        Me.MSG2.AccessibleName = Nothing
         resources.ApplyResources(Me.MSG2, "MSG2")
-        Me.MSG2.BackgroundImage = Nothing
-        Me.MSG2.Font = Nothing
         Me.MSG2.Name = "MSG2"
         '
         'MSG1
         '
-        Me.MSG1.AccessibleDescription = Nothing
-        Me.MSG1.AccessibleName = Nothing
         resources.ApplyResources(Me.MSG1, "MSG1")
-        Me.MSG1.BackgroundImage = Nothing
-        Me.MSG1.Font = Nothing
         Me.MSG1.Name = "MSG1"
         '
         'Label6
         '
-        Me.Label6.AccessibleDescription = Nothing
-        Me.Label6.AccessibleName = Nothing
         resources.ApplyResources(Me.Label6, "Label6")
-        Me.Label6.Font = Nothing
         Me.Label6.Name = "Label6"
         '
         'Label7
         '
-        Me.Label7.AccessibleDescription = Nothing
-        Me.Label7.AccessibleName = Nothing
         resources.ApplyResources(Me.Label7, "Label7")
-        Me.Label7.Font = Nothing
         Me.Label7.Name = "Label7"
         '
         'Label9
         '
-        Me.Label9.AccessibleDescription = Nothing
-        Me.Label9.AccessibleName = Nothing
         resources.ApplyResources(Me.Label9, "Label9")
-        Me.Label9.Font = Nothing
         Me.Label9.Name = "Label9"
         '
         'RadioPLUS
         '
-        Me.RadioPLUS.AccessibleDescription = Nothing
-        Me.RadioPLUS.AccessibleName = Nothing
         resources.ApplyResources(Me.RadioPLUS, "RadioPLUS")
-        Me.RadioPLUS.BackgroundImage = Nothing
-        Me.RadioPLUS.Font = Nothing
         Me.RadioPLUS.Name = "RadioPLUS"
         Me.RadioPLUS.UseVisualStyleBackColor = True
         '
         'Label8
         '
-        Me.Label8.AccessibleDescription = Nothing
-        Me.Label8.AccessibleName = Nothing
         resources.ApplyResources(Me.Label8, "Label8")
-        Me.Label8.Font = Nothing
         Me.Label8.Name = "Label8"
         '
         'ButtonOK
         '
-        Me.ButtonOK.AccessibleDescription = Nothing
-        Me.ButtonOK.AccessibleName = Nothing
         resources.ApplyResources(Me.ButtonOK, "ButtonOK")
-        Me.ButtonOK.BackgroundImage = Nothing
-        Me.ButtonOK.Font = Nothing
         Me.ButtonOK.Name = "ButtonOK"
         Me.ButtonOK.UseVisualStyleBackColor = True
         '
         'ButtonNew
         '
-        Me.ButtonNew.AccessibleDescription = Nothing
-        Me.ButtonNew.AccessibleName = Nothing
         resources.ApplyResources(Me.ButtonNew, "ButtonNew")
-        Me.ButtonNew.BackgroundImage = Nothing
-        Me.ButtonNew.Font = Nothing
         Me.ButtonNew.Name = "ButtonNew"
         Me.ButtonNew.UseVisualStyleBackColor = True
         '
         'ButtonDelete
         '
-        Me.ButtonDelete.AccessibleDescription = Nothing
-        Me.ButtonDelete.AccessibleName = Nothing
         resources.ApplyResources(Me.ButtonDelete, "ButtonDelete")
-        Me.ButtonDelete.BackgroundImage = Nothing
-        Me.ButtonDelete.Font = Nothing
         Me.ButtonDelete.Name = "ButtonDelete"
         Me.ButtonDelete.UseVisualStyleBackColor = True
         '
         'ButtonEdit
         '
-        Me.ButtonEdit.AccessibleDescription = Nothing
-        Me.ButtonEdit.AccessibleName = Nothing
         resources.ApplyResources(Me.ButtonEdit, "ButtonEdit")
-        Me.ButtonEdit.BackgroundImage = Nothing
-        Me.ButtonEdit.Font = Nothing
         Me.ButtonEdit.Name = "ButtonEdit"
         Me.ButtonEdit.UseVisualStyleBackColor = True
         '
         'FilterDialog
         '
-        Me.AccessibleDescription = Nothing
-        Me.AccessibleName = Nothing
         resources.ApplyResources(Me, "$this")
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackgroundImage = Nothing
         Me.CancelButton = Me.ButtonClose
         Me.ControlBox = False
         Me.Controls.Add(Me.ComboTabs)
@@ -331,9 +233,7 @@ Partial Class FilterDialog
         Me.Controls.Add(Me.ButtonDelete)
         Me.Controls.Add(Me.ButtonEdit)
         Me.Controls.Add(Me.ButtonClose)
-        Me.Font = Nothing
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
-        Me.Icon = Nothing
         Me.KeyPreview = True
         Me.MaximizeBox = False
         Me.MinimizeBox = False
