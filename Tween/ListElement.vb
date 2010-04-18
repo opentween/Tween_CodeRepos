ea238890112ec4bcd3adf0972573391251965ff4
kiri_feather <kiri_feather@users.sourceforge.jp>
2010-04-18 22:40:30 +0000

    リスト選択画面作成中
    git-svn-id: http://svn.sourceforge.jp/svnroot/tween/trunk@158 e39ad16e-3079-482e-bb30-4b4d378143b6

--- Tween/ListElement.vb
+++ Tween/ListElement.vb
@@ -2,10 +2,15 @@
     Public Id As Long = 0
     Public Name As String = ""
     Public Description As String = ""
+    Public Slug As String = ""
     Public IsPublic As Boolean = True
     Public SubscriberCount As Integer = 0   '購読者数
     Public MemberCount As Integer = 0   'リストメンバ数
     Public UserId As Long = 0
     Public Username As String = ""
     Public Nickname As String = ""
+
+    Public Overrides Function ToString() As String
+        Return "@" + Username + "/" + Name
+    End Function
 End Class
